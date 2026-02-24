5c14064966839d1b2dd849311291103237864eaf
Jasper <jasper.kooij2@gmail.com>
2026-02-24 15:43:27 +0100

    Implemented feedback

--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,23 @@
-// window.addEventListener("resize", function (e) {
-
-// });
-
 function navigationBarOpen() {
-
     const navBar = document.querySelector(".navigation-bar");
-    const navItems = document.getElementById("navigation-items-bar");
+    const navItems = document.querySelector(".navigation-items");
     const navCloseButtonHam = document.querySelector(".navigation-open");
     const navCloseButtonX = document.querySelector(".navigation-close");
     
-    console.log("Open!");
-
     navCloseButtonHam.classList.toggle("hidden");
     navCloseButtonX.classList.toggle("hidden");
-
-
     navItems.classList.toggle("hidden");
-
     navBar.classList.toggle("full");
-
-
-
 }
 
 function navigationBarClose() {
-
     const navBar = document.querySelector(".navigation-bar");
-    const navItems = document.getElementById("navigation-items-bar");
+    const navItems = document.querySelector(".navigation-items");
     const navCloseButtonHam = document.querySelector(".navigation-open");
     const navCloseButtonX = document.querySelector(".navigation-close");
 
-    console.log("Close!");
-
     navCloseButtonHam.classList.toggle("hidden");
     navCloseButtonX.classList.toggle("hidden");
-
-
     navItems.classList.toggle("hidden");
-
     navBar.classList.toggle("full");
-
-
-
 }
