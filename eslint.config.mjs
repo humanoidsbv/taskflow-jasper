4582d56c7867d4e9adcac8c731222a60f78d7510
Jasper <jasper.kooij2@gmail.com>
2026-02-25 13:46:25 +0100

    Initial commit for Next.js and React app

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+  {
+    rules: {
+      // "warn" shows a yellow underline; "error" breaks the build
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+    },
+  },
+]);
+
+export default eslintConfig;
