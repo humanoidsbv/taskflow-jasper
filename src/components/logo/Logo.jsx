719b1f0b29988eb8da21a4a6d53e961e59304219
Jasper <jasper.kooij2@gmail.com>
2026-02-27 14:59:18 +0100

    Feedback verwerkings

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import logo from "../../../public/logos/taskflow-logo.svg";
+import logo from "@/public/logos/taskflow-logo.svg";
 
 import styles from "./Logo.module.css";
 
