3ddf781990535ba4cb643035ba0e5a70bf1c0a0c
Jasper <jasper.kooij2@gmail.com>
2026-02-27 14:58:21 +0100

    Feedback for code netheid

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./Logo.module.css";
-import logo from "../../../public/logos/taskflow-logo.svg";
+import logo from "@/public/logos/taskflow-logo.svg";
 import Link from "next/link";
 
 export const Logo = ({ onClick }) => {
--- src/components/subheader/Subheader.jsx
+++ src/components/subheader/Subheader.jsx
@@ -1,51 +1,49 @@
 "use client";
 
-import Image from "next/image";
-import { Button } from "../button/Button";
-import styles from "./Subheader.module.css";
-import buttonIcon from "@/public/icons/plus-icon.svg";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
+
+import { Button } from "../button/Button";
+import buttonIcon from "@/public/icons/plus-icon.svg";
 import destinations from "@/services/destinations.json" with { type: "json" };
+
+import styles from "./Subheader.module.css";
 
 export const Subheader = () => {
   const pathname = usePathname();
   if (pathname === "/") return;
-  const destination = destinations.filter(
+  const currentDestination = destinations.filter(
     (entry) => entry.href === pathname,
   )[0];
-  const variant = destination.variant;
+  const variant = currentDestination.variant;
 
   let buttonText = "";
 
   switch (variant) {
     case "event":
-      buttonText = "Add new events";
+      buttonText = "New event";
       break;
     case "member":
-      buttonText = "Add members";
+      buttonText = "Add member";
       break;
     default:
       buttonText = "Default";
       break;
   }
 
-  console.log(pathname);
-  console.log(destinations);
-  console.log(destination);
-
-  // console.log(destination[0]);
-
   return (
     <div className={styles.subheader}>
       <div className={styles.titles}>
-        <h5 className={styles.title}>{destination.name}</h5>
+        <h5>{currentDestination.name}</h5>
         <div className={styles.divider}></div>
-        <p className={styles.subtitle}>{destination.subtitle}</p>
+        <p className={styles.subtitle}>
+          destination subtitle{currentDestination.subtitle}
+        </p>
       </div>
       <Button>
         <Image
           src={buttonIcon}
-          alt={`${destination.variant === "member" ? "Add member" : "Add new events"} `}
+          alt={`${currentDestination.variant === "member" ? "Add member" : "Add new events"} `}
         />
         <span className={styles.text}>{buttonText}</span>
       </Button>
