5ce47a51fd01d5c3aee104195715826281e4b5b3
Jasper <jasper.kooij2@gmail.com>
2026-03-06 10:14:51 +0100

    Expanded mock data

--- services/timeEntries.ts
+++ services/timeEntries.ts
@@ -11,4 +11,28 @@ export const timeEntries = [
         startTimestamp: '2022-09-24T16:00:00.000Z',
         stopTimestamp: '2022-09-25T18:00:00.000Z'
     },
+    {
+        id: 3,
+        client: 'Jupiler',
+        startTimestamp: '2022-09-25T16:00:00.000Z',
+        stopTimestamp: '2022-09-25T18:00:00.000Z'
+    },
+    {
+        id: 4,
+        client: 'Grolsch',
+        startTimestamp: '2022-09-26T16:00:00.000Z',
+        stopTimestamp: '2022-09-26T18:00:00.000Z'
+    },
+    {
+        id: 5,
+        client: 'Hertog Jan',
+        startTimestamp: '2022-09-27T16:00:00.000Z',
+        stopTimestamp: '2022-09-27T18:00:00.000Z'
+    },
+    {
+        id: 6,
+        client: 'Bavaria',
+        startTimestamp: '2022-09-28T16:00:00.000Z',
+        stopTimestamp: '2022-09-28T18:00:00.000Z'
+    },
 ];
