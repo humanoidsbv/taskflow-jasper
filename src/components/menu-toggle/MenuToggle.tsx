3e56ac6226e3b6fe00560cb45096975a59a2efe3
Jasper <jasper.kooij2@gmail.com>
2026-03-13 12:02:51 +0100

    Toggle between HamburgerIcon and CloseIcon in Header

--- src/components/menu-toggle/MenuToggle.tsx
+++ src/components/menu-toggle/MenuToggle.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 
 import CloseIcon from "@/public/icons/close.svg";
-import hamburgerIcon from "@/public/icons/hamburger-icon.svg";
+import HamburgerIcon from "@/public/icons/hamburger-icon.svg";
 
 import styles from "./MenuToggle.module.css";
 
@@ -14,10 +14,14 @@ interface MenuToggleProps {
 
 export const MenuToggle = ({ isMenuOpen, toggleMenu }: MenuToggleProps) => (
   <button className={styles.button} onClick={() => toggleMenu(!isMenuOpen)}>
-    <CloseIcon
-      alt="Toggle the navigation menu"
-      className={styles.image}
-      // src={isMenuOpen ? closeIcon : hamburgerIcon}
-    />
+    {isMenuOpen ? (
+      <CloseIcon
+        alt="Close the navigation menu"
+        className={styles.image}
+        // src={isMenuOpen ? closeIcon : hamburgerIcon}
+      />
+    ) : (
+      <HamburgerIcon alt="Open the navigation menu" className={styles.image} />
+    )}
   </button>
 );
