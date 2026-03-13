4fc627f960f37c192ea9a550df4b425f81764823
Jasper <jasper.kooij2@gmail.com>
2026-03-13 11:14:25 +0100

    Add TimeEntryForm component

---
+++ src/components/time-entry-form/TimeEntryForm.tsx
@@ -0,0 +1,8 @@
+import Form from "next/form";
+import styles from "./TimeEntryForm.module.css";
+
+interface TimeEntryFormProps {}
+
+export const TimeEntryForm = () => {
+  return <Form action="/search"></Form>;
+};
