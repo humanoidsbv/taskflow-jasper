a738b5efcbd2bd6c2a8a1668ac9f74206e15171c
Jasper <jasper.kooij2@gmail.com>
2026-03-16 11:31:49 +0100

    Server action format form data

--- src/components/time-entry-form/actions.ts
+++ src/components/time-entry-form/actions.ts
@@ -5,5 +5,13 @@ export const createCalendarEvent = async (
 ): Promise<void> => {
   const data = Object.fromEntries(formData);
 
-  console.table(data);
+  const formattedData = {
+    client: data.client,
+    startTimestamp: new Date(`${data.date}T${data.startDate}`).toISOString(),
+    stopTimestamp: new Date(`${data.date}T${data.stopDate}`).toISOString(),
+    billable: data.activity.toString().split("-")[1] === "billable",
+    department: data.activity.toString().split("-")[0],
+  };
+
+  console.table(formattedData);
 };
