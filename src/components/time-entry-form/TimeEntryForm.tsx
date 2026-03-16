945493cf491475093c159e53e9e9fcbc4fc14771
Jasper <jasper.kooij2@gmail.com>
2026-03-16 09:55:33 +0100

    Add correct types and data handling, start on handleChange event

--- src/components/time-entry-form/TimeEntryForm.tsx
+++ src/components/time-entry-form/TimeEntryForm.tsx
@@ -6,21 +6,32 @@ import { Button } from "@/components/button/Button";
 import { createCalendarEvent } from "./actions";
 
 import styles from "./TimeEntryForm.module.css";
+import { useState } from "react";
 
 interface TimeEntryFormProps {
   closeModal: () => void;
 }
 
 export const TimeEntryForm = ({ closeModal }: TimeEntryFormProps) => {
+  const [totalHours, setTotalHours] = useState("00:00");
+
+  function handleChange(event: React.SyntheticEvent<HTMLFormElement>) {
+    const formData = new FormData(event.currentTarget);
+
+    setTotalHours("01:00");
+    console.log(formData);
+  }
+
   return (
     <>
       <span className={styles.title}>New event</span>
-      <Form action={createCalendarEvent}>
+      <Form action={createCalendarEvent} onChange={handleChange}>
         <input name="client" required type="text" />
         <input name="activity" required type="text" />
-        <input name="date" type="text" />
-        <input name="fromDate" type="text" />
-        <input name="toDate" type="text" />
+        <input name="date" type="date" />
+        <input name="fromDate" type="time" />
+        <input name="toDate" type="time" />
+        <span>{totalHours}</span>
         <Button variant="secondary" onClick={closeModal}>
           Cancel
         </Button>
