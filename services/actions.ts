c23f7fec530a5b0680910b0443cfc6dcb95d2adf
Jasper <jasper.kooij2@gmail.com>
2026-03-18 16:45:14 +0100

    Start on database connection, calendarpage is client for now

--- services/actions.ts
+++ services/actions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { TimeEntryData } from "@/types/dataTypes";
 import { z } from "zod";
 
 const timeToMinutes = (time: string) => {
@@ -85,3 +86,28 @@ export const createCalendarEvent = async (
 
   return { message: "Event created" };
 };
+
+export async function getTimeEntries(): Promise<TimeEntryData[]> {
+  try {
+    const response = await fetch("http://localhost:3004/time-entries", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.status === 404) {
+      throw new NotFoundError("Pls help I am under da water");
+    }
+    return response.json();
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+class NotFoundError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "NotFoundError";
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 import { Subheader } from "@/components/subheader/Subheader";
 import { TimeEntries } from "@/components/time-entries/TimeEntries";
-import { timeEntries } from "@/services/fixtures";
+import { getTimeEntries } from "@/services/actions";
+import { TimeEntryData } from "@/types/dataTypes";
 
 export default function CalendarPage() {
+  const [timeEntries, setTimeEntries] = useState<TimeEntryData[]>([]);
+
+  async function fetchTimeEntries() {
+    setTimeEntries(await getTimeEntries());
+  }
+
   const subtitle = `${timeEntries.length} event${
     timeEntries.length === 1 ? "" : "s"
   }`;
+
+  useEffect(() => {
+    fetchTimeEntries();
+  }, []);
 
   return (
     <>
