38c68ed3a8189ca4b2b21194e6a89a90fb3abc93
Jasper <jasper.kooij2@gmail.com>
2026-03-19 14:28:53 +0100

    Move ordering of timeEntries to api request

--- services/timeEntries.ts
+++ services/timeEntries.ts
@@ -2,15 +2,19 @@ import { TimeEntryData } from "@/types/dataTypes";
 
 export async function getTimeEntries(): Promise<TimeEntryData[]> {
   try {
-    const response = await fetch("http://localhost:3004/time-entries", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
+    const response = await fetch(
+      "http://localhost:3004/time-entries?_sort=-startTimestamp",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
       },
-    });
+    );
     if (response.status === 404) {
       throw new NotFoundError("Pls help I am under da water");
     }
+    console.log("succes");
     return response.json();
   } catch (error) {
     console.error(error);
